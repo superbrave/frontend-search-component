ed6826bcaba9ca04c3fe96b11aeda7c7479c993f
Ricardo <Rivadek@outlook.com>
2021-08-20 09:08:15 +0200

    [DV-1206] Made it possible to add country to search component (#29)

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 import "regenerator-runtime/runtime";
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
 
-function getConnector(env, lang) {
+function getConnector(env, lang, country) {
+  const countrySuffix = country ? `-${country}` : "";
+
   return new AppSearchAPIConnector({
-    engineName: "dokteronline-" + env + "-" + lang,
+    engineName: "dokteronline-" + env + "-" + lang + countrySuffix,
     hostIdentifier: "host-h892yb",
     endpointBase:
       "https://2f98e35e63fa458c89886be577b61301.ent-search.eu-central-1.aws.cloud.es.io",
@@ -11,10 +13,10 @@ function getConnector(env, lang) {
   });
 }
 
-export default function (env, lang) {
+export default function (env, lang, country) {
   return {
     debug: env === "dev",
-    apiConnector: getConnector(env, lang),
+    apiConnector: getConnector(env, lang, country),
     searchQuery: {
       resultsPerPage: 50,
       disjunctiveFacets: ["type"],
